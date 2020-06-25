e1e87037bda6131c0f8cfab01b09fd7a9b60031e
ianzures <i.anzures514@gmail.com>
2020-06-25 16:00:49 -0400

    Incomplete Front end.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,48 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class zipSearch extends React.Component {
+
+}
+
+const divStyle = {
+    fontSize: '300%',
+    fontWeight: 'bold',
+    fontFamily: "Times New Roman",
+    color: '#ffffcc'
+}
+
+const boxColor = {
+    paddingTop: '50px',
+    paddingBottom: '40px',
+    paddingLeft: '80px',
+    backgroundColor: '#ff0066',
+    marginLeft: '50px',
+    marginTop: '20px',
+    marginRight: '50px'
+}
+const search = {
+    fontFamily: 'Impact',
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '20px'
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div>
+        <div style={boxColor}>
+            <div style={divStyle} >
+                Zip Code Search
+            </div>
+        </div>
+        <form style={search}>
+            <label>
+                Zip Code:
+                <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    </div>,
   document.getElementById('root')
 );
 
