49490d40dff4c46161ac37c7320f96d11bc061e0
ianzures <i.anzures514@gmail.com>
2020-07-02 12:41:42 -0400

    Added error message and list of desired city characteristics.

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ const boxColor = {
     paddingBottom: '40px',
     paddingLeft: '80px',
     backgroundColor: '#ff0066',
-    marginLeft: '50px',
-    marginTop: '20px',
-    marginRight: '50px'
+    //marginLeft: '50px',
+    //marginTop: '20px',
+    //marginRight: '50px'
 }
 const search = {
     fontFamily: 'Impact',
@@ -30,72 +30,77 @@ const search = {
 
 const result = {
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderStyle: 'double',
+    marginLeft: '400px',
+    marginTop: '30px',
+    marginRight: '400px',
+    borderRadius: '10px'
+}
+
+const cellHead = {
+    //backgroundColor : 'grey'
 }
 
 export default class ZipSearch extends React.Component{
-    state = {
-        cities: [],
-        zip : '11435'
+    constructor(props) {
+        super(props);
+        this.state = {
+            cities: [],
+            zip: '',
+        };
     }
+    
 
     handleSubmit = event => {
+
         event.preventDefault();
 
-        axios.get("http://ctp-zip-api.herokuapp.com/zip/" + this.state.zip).
-            then(results => {
+        this.setState({ els:[] });
+        this.setState({ cities:[] });
+
+
+        axios.get("http://ctp-zip-api.herokuapp.com/zip/" + this.state.zip).then(results => {
                 this.setState({ cities: results.data });
-                console.log(this.state.cities);
-            });
+            }).catch(function (err) {
+                alert("Zip code not found.");
+            }
+        );
     }
 
     handleChange = event => {
         this.setState({ zip: event.target.value });
-    };
-
-    /*constructor(props) {
-        super(props);
-        this.url = "http://ctp-zip-api.herokuapp.com/zip/10016";
-        this.state = {
-           // zip: "",
-            cities: []
-        };
     }
-
-    
-
-    componentDidMount() {
-        axios.get(this.props.url + this.state.zip)
-            .then(res => {
-                const cityList = res.data;
-                this.setState({ cityList });
-            })
-
-
-
-
-                
-    }*/
 
     render() {
         return (
             <React.Fragment>
-                {/* --------------------------- */}
                 <div style={boxColor}>
                     <div style={divStyle} >
-                        Zip Code Search
+                       Zip Code Search 
                     </div>
                 </div>
 
                 <form style={search} onSubmit={this.handleSubmit}>
                     <label >
-                        Zip Code: <input type="text" name="zip" onChange={this.handleChange}/>
+                        Zip Code: <input type="text" name="zip" onChange={this.handleChange} />
                     </label>
                 </form>
-
+             
                 <div>
-                    {this.state.cities.map(city => <p style={result}>{city.City},{city.State}</p>)}
-                </div>
+                    {this.state.cities.map(city =>
+                        <div key={city.RecordNumber} style={result}>
+                            <header style={cellHead}>{city.City},{city.State}</header>
+                            <ul>
+                                <li>State: {city.State}</li>
+                                <li>Location: ({city.Lat},{city.Long})</li>
+                                <li>Population (estimated): {city.EstimatedPopulation}</li>
+                                <li>Total wages: {city.TotalWages}</li>
+
+                            </ul>
+                        </div>)
+                    }
+                </div> 
 
             </React.Fragment>
         )
